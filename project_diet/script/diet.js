d6ca337ddf134810c6daf07a56cb72f1b9337d65
Leeyeyun <165612381+Leeyeyun@users.noreply.github.com>
2024-05-23 16:25:58 +0900

    다이어트체중계 프로젝트 추가

---
+++ project_diet/script/diet.js
@@ -0,0 +1,22 @@
+/* 
+계산식) 적정체중 = (본인신장-100)*0.9
+- 현재 키와 몸무게를 자유롭게 입력하고
+- 버튼을 눌렀을 때 최종 목표 칸에 메세지 출력하기
+결과예시) 적정체중은 ?kg이며 ?kg 초과되셨습니다.
+*/
+const height = document.querySelector("#height")
+const weight = document.querySelector("#weight")
+/* const goals = document.querySelector("#goals") */
+const btn = document.querySelector("#result")
+console.log(height, weight, btn)
+btn.addEventListener('click',function(){
+    let user_h = Number(height.value)
+    let user_w = Number(weight.value)
+    let calc = (user_h - 100) * 0.9
+    let message = `너에게 완벽한 체중은
+${calc}kg 이다.
+${user_w - calc}kg 이나 초과했네?
+오늘 야식은 금지다.`;
+    document.getElementById('goals').value = message;
+    console.log(message);
+})
